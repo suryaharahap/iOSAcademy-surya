36abec3eec4ef1c01c89490fb3c0e8ead0a52d91
suryaharahap <suryaharahap18@gmail.com>
2022-08-21 21:48:44 +0700

    Add: Learn auto layout

---
+++ MyFunios/MyFunios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFunios
+//
+//  Created by Surya on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
