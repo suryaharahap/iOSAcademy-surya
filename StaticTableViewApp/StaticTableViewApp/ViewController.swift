25e880cbbdeb5a7f5ed31d00345180c1bc50f328
suryaharahap <suryaharahap18@gmail.com>
2022-08-22 07:37:18 +0700

    Add: Implementing static table views

--- StaticTableViewApp/StaticTableViewApp/ViewController.swift
+++ StaticTableViewApp/StaticTableViewApp/ViewController.swift
@@ -1,19 +1,12 @@
-//
-//  ViewController.swift
-//  StaticTableViewApp
-//
-//  Created by Surya on 22/08/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
+  
+  
 }
 
