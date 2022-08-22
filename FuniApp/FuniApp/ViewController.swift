6cb52ebf5b821649001280d5034705fe26cf8dcc
suryaharahap <suryaharahap18@gmail.com>
2022-08-22 08:14:32 +0700

    Initial project dynamic table view

---
+++ FuniApp/FuniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FuniApp
+//
+//  Created by Surya on 22/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
