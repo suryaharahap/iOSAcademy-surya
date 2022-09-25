2b31690b930b5cd6161d8b7edc7428da6de159b1
suryaharahap <suryaharahap18@gmail.com>
2022-09-25 21:16:21 +0700

    Add: OneMovie initialite repository

---
+++ OneMovie/OneMovie/OneMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneMovieApp.swift
+//  OneMovie
+//
+//  Created by Surya on 25/09/22.
+//
+
+import SwiftUI
+
+@main
+struct OneMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
