03a0429601f31f6997050d3031bb3ebb6ab2c284
suryaharahap <suryaharahap18@gmail.com>
2022-10-01 05:51:40 +0700

    Initialize repository swift ui app quickcard

---
+++ QuickCardsFun/QuickCardsFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickCardsFun
+//
+//  Created by Surya on 01/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickCardsFun/QuickCardsFun/QuickCardsFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCardsFunApp.swift
+//  QuickCardsFun
+//
+//  Created by Surya on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickCardsFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
