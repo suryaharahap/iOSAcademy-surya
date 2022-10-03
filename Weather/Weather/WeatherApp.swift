514b6eea6e03d616c56236c8c83fa90feb8379ac
suryaharahap <suryaharahap18@gmail.com>
2022-10-03 10:08:50 +0700

    Initialize repository weatherApp

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Surya on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
