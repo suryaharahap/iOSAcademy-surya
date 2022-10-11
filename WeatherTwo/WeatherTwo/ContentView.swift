46382a9fd2eda594693821941737affd57993ece
suryaharahap <suryaharahap18@gmail.com>
2022-10-11 09:41:40 +0700

    Add: Initialize project weatherTwo

---
+++ WeatherTwo/WeatherTwo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherTwo
+//
+//  Created by Surya on 11/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherTwo/WeatherTwo/WeatherTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTwoApp.swift
+//  WeatherTwo
+//
+//  Created by Surya on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
