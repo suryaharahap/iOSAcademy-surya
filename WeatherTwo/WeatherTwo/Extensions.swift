9b60d4e248cf199bc0041d8a473853dfbd4796b1
suryaharahap <suryaharahap18@gmail.com>
2022-10-13 20:29:24 +0700

    Add: Extension file

---
+++ WeatherTwo/WeatherTwo/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherTwo
+//
+//  Created by Surya on 13/10/22.
+//
+
+import Foundation
+
+extension Double {
+  func roundDouble() -> String {
+    return String(format: "%.0f", self)
+  }
+}
