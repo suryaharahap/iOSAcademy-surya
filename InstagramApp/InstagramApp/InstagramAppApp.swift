f749f640ba7477dd41d22e685cb185eaa11364fd
suryaharahap <suryaharahap18@gmail.com>
2022-11-07 01:32:15 +0700

    Initialize setup repository InstagramApp

---
+++ InstagramApp/InstagramApp/InstagramAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramAppApp.swift
+//  InstagramApp
+//
+//  Created by Surya on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
